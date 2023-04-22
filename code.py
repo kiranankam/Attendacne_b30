53562b5c438ca89907d37f477ff1792efee85e56
kiranankam <84177904+kiranankam@users.noreply.github.com>
2023-04-22 13:47:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import cv2
 import numpy as np
 import face_recognition
@@ -5,16 +6,64 @@ import os
 from datetime import datetime
 
 
-path = 'images'
-images = []
-classNames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
-    curImg = cv2.imread('{}/{}'.format(path,cl))
-    images.append(curImg)
-    classNames.append(os.path.splitext(cl)[0])
-print(classNames)
+def main():
+    st.title("Face Recognition App")
+
+    # Load images
+    path = 'images'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    for cl in myList:
+        curImg = cv2.imread(os.path.join(path, cl))
+        images.append(curImg)
+        classNames.append(os.path.splitext(cl)[0])
+
+    # Find encodings
+    encodeListKnown = findEncodings(images)
+    st.write('Encoding Complete')
+
+    # Initialize video capture
+    cap = cv2.VideoCapture(0)
+
+    # Loop through frames
+    while True:
+        # Read frame from video capture
+        success, img = cap.read()
+
+        # Resize and convert to RGB
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        # Find faces and encodings
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        # Loop through detected faces
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendance(name)
+
+        # Show video stream and recognized names
+        st.image(img, channels="RGB")
+        st.write(f"Recognized name: {name}")
+
+        # Check for keyboard interrupt
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release video capture and close windows
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def findEncodings(images):
@@ -27,45 +76,4 @@ def findEncodings(images):
 
 
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-            now = datetime.now()
-            dtString = now.strftime('%H:%M:%S')
-            f.writelines('\n{},{}'.format(name,dtString))
-
-
-
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
-
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        matchIndex = np.argmin(faceDis)
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
-
-
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+    with open('Attendance.csv', 'r+') as
